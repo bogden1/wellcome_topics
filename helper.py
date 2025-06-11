e8c3906303945b91a3d93f8187b7c18acb4816ad
Bernard Ogden <bernard.ogden@nationalarchives.gov.uk>
2025-06-11 19:35:30 +0100

    Simplify the dual counting of entries and hits

--- helper.py
+++ helper.py
@@ -35,45 +35,29 @@ def list_by_jsonpath(jsonpath, jsonl_list):
 
 #Return a counter keyed on the value of query that matched the elements of jsonl_list
 def count(jsonpath, jsonl_list):
-  empty = 0
   counter = Counter()
   searcher = json_parse(jsonpath)
   for jsonl in jsonl_list:
     results = searcher.find(jsonl)
     if len(results) == 0:
-      empty += 1
+      counter[''] += 1
     else:
       for result in results: #we should have a list of DatumInContext
                              #this function assumes the value will be hashable, so it does not handle all queries
                              #for example, it will not work if "value" is a dict or a list
         counter[result.value] += 1
-  return empty, counter
+  return counter
 
 #Call counter with jsonpath and jsonl_list
 #Then report on what was counted, per entry in the jsonl_list and (if different total) per hit
 def dumpCount(jsonpath, jsonl_list, min_entry_proportion = 0):
-  emptyCount, counter = count(jsonpath, jsonl_list)
+  counter = count(jsonpath, jsonl_list)
   list_total = len(jsonl_list)
   query_total = counter.total()
-  below_min = 0
   for k, v in counter.most_common():
     list_proportion = v/list_total
     if list_proportion >= min_entry_proportion:
       output = f'{v:4}/{list_total} entries ({100 * v/list_total:3.0f}%)'
       if list_total != query_total:
         output += f'; {v:4}/{query_total} hits ({100 * v/query_total:3.0f}%)'
-      print(output, k)
-    else:
-      below_min += 1
-  if emptyCount > 0 or below_min > 0:
-    print() #Space things out if we need to append this stuff
-  if emptyCount > 0:
-    output = f'{emptyCount:4}/{list_total} entries ({100 * emptyCount/list_total:3.0f}%)'
-    if list_total != query_total:
-      output += f'; {emptyCount:4}/{query_total} hits ({100 * emptyCount/query_total:3.0f}%)'
-    print(output, 'have no value')
-  if below_min > 0:
-    output = f'{below_min:4}/{list_total} entries ({100 * below_min/list_total:3.0f}%)'
-    if list_total != query_total:
-      output += f'; {below_min:4}/{query_total} hits ({100 * below_min/query_total:3.0f}%)'
-    print(output, f'non-empty results hidden as below minimum entry proportion of {min_entry_proportion * 100:.0f}%')
+      print(output, '<no value>' if k == '' else k)
